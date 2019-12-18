1cbf71e4ffc40747b130988d4156c0459c4421ae
enochc <enochc@kengarff.com>
2019-12-18 09:05:27 -0700

    fixing paths and config files

--- config/envars.php
+++ config/envars.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'icons_path' => storage_path() . env('ICONS_FILE_PATH', '/app/public/icons/'),
+    'icons_path' => resource_path() . env('ICONS_FILE_PATH', '/icons/'),
     'profiles_path' => storage_path() . env('PROFILES_FILE_PATH', '/app/public/pfpics/'),
     'tweet_media_path' => storage_path() . env('TWEET_MEDIA_FILE_PATH', '/app/public/pfpics/'),
 ];
